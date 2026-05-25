9b87da96c4bd2db7d3fed502c1d83541d267a5e8
maromo71 <professormoraes@gmail.com>
2026-05-25 16:47:38 -0300

    Aula do dia 25 de maio

---
+++ aula1805/meuapp5/src/App.jsx
@@ -0,0 +1,9 @@
+import ZodiacSigns from "./pages/ZodiacSigns";
+
+export default function App(){
+    return(
+        <div>
+            <ZodiacSigns />
+        </div>
+    )
+}
---
+++ aula1805/meuapp5/src/components/AppTitle.jsx
@@ -0,0 +1,9 @@
+export default function AppTitle(){
+    return (
+        <div className="box">
+            <p className="texto">
+                Baralho de Signos
+            </p>
+        </div>
+    )
+}
